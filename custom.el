f62aef9236e9231751548b9edf4bce97993fa1de
niels <niels@algon.dk>
2020-12-21 10:34:42 +0100

    Finalized org-roam and Elfeed-dashboard

--- custom.el
+++ custom.el
@@ -4,12 +4,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(comint-terminfo-terminal "xterm-256color")
+ '(company-backends
+   '(company-lsp
+     (company-files company-keywords company-capf)
+     (company-abbrev company-dabbrev)))
  '(dashboard-buffer-last-width 214)
  '(doom-modeline-gnus-timer 0)
  '(eww-bookmarks-directory "~/.emacs.d/private/")
  '(exec-path
    '("/bin/" "/usr/bin/" "/usr/local/bin/" "/usr/local/texlive/bin/" "/opt/local/bin/" "/Library/TeX/texbin/" "/usr/sbin/" "/sbin/" "/opt/X11/bin/" "/Applications/Emacs.app/Contents/MacOS/libexec/" "/usr/local/Cellar/pyenv-virtualenv/1.1.5/shims/" "/usr/local/opt/coreutils/libexec/gnubin/"))
  '(file-template-insert-automatically t)
+ '(flycheck-checker-error-threshold nil)
  '(focus-follows-mouse 'auto-raise)
  '(font-latex-fontify-sectioning 1.2)
  '(helm-allow-mouse t)
@@ -17,10 +22,10 @@
  '(mouse-wheel-progressive-speed nil)
  '(mouse-yank-at-point t)
  '(nxml-bind-meta-tab-to-complete-flag nil)
- '(preview-TeX-style-dir "/Users/niels/.emacs.d/straight/build/auctex/latex" t)
+ '(preview-TeX-style-dir "/Users/niels/.emacs.d/straight/build/auctex/latex")
  '(projectile-known-projects-file "/Users/niels/.emacs.d/private/projectile-bookmarks.eld")
  '(projectile-mode-line-prefix " ✐")
- '(reftex-plug-into-AUCTeX t t)
+ '(reftex-plug-into-AUCTeX t)
  '(sane-term-shell-command "/bin/zsh")
  '(set-mark-command-repeat-pop t)
  '(smartparens-global-mode t)
